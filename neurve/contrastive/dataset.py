84ef7e79ba80dbd81b21f37611380493eef491e0
Eric O. Korman <eric.korman@gmail.com>
2024-05-16 14:46:44 -0500

    update simclr dataset

--- neurve/contrastive/dataset.py
+++ neurve/contrastive/dataset.py
@@ -1,9 +1,12 @@
+from typing import Tuple
+
 from torch.utils.data import Dataset
 from torchvision.transforms import (
     ColorJitter,
     Compose,
     RandomApply,
     RandomGrayscale,
+    RandomHorizontalFlip,
     RandomResizedCrop,
     ToTensor,
 )
@@ -11,15 +14,14 @@ from torchvision.transforms import (
 
 class SimCLRDataset(Dataset):
     def __init__(
-        self, base_dataset, img_size=(32, 32), color_dist_strength=0.5
+        self,
+        base_dataset,
+        img_size=(32, 32),
+        color_dist_strength=0.5,
+        crop_scale: Tuple[float, float] = (0.08, 1.0),
+        horizontal_flip_prob: float = 0.0,
+        grayscale_prob: float = 0.2,
     ):
-        """
-        Parameters
-        ----------
-        base_dataset : Dataset
-        img_size : tuple
-        color_dist_strength : float
-        """
         self.base_dataset = base_dataset
         # get color distortion random transform, Color distortion (see Pytorch
         # pseudocode in arXiv:2002.05709)
@@ -30,15 +32,21 @@ class SimCLRDataset(Dataset):
             0.2 * color_dist_strength,
         )
         rnd_color_jitter = RandomApply([color_jitter], p=0.8)
-        rnd_gray = RandomGrayscale(p=0.2)
-        self.transform = Compose(
-            [
-                RandomResizedCrop(img_size),
-                rnd_color_jitter,
-                rnd_gray,
-                ToTensor(),
-            ]
-        )
+        rnd_gray = RandomGrayscale(p=grayscale_prob)
+        rnd_horizontal_flip = RandomHorizontalFlip(horizontal_flip_prob)
+
+        transforms = [
+            RandomResizedCrop(img_size, scale=crop_scale),
+            rnd_color_jitter,
+        ]
+
+        if grayscale_prob > 0.0:
+            transforms.append(rnd_gray)
+        if horizontal_flip_prob > 0.0:
+            transforms.append(rnd_horizontal_flip)
+
+        transforms.append(ToTensor())
+        self.transform = Compose(transforms)
 
     def __getitem__(self, index):
         return (
